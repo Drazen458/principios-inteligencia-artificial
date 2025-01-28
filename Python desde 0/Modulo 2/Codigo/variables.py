766719768c05ffe9bec6f53bac9082e67752989f
Drazen458 <valentinbarberis211@gmail.com>
2025-01-28 20:50:41 -0300

    Add file

---
+++ Python desde 0/Modulo 2/Codigo/variables.py
@@ -0,0 +1,28 @@
+# VARIABLES
+
+# Contenedores que almacenan datos que pueden cambiar durante la ejecucion de un programa.
+# Cada variable tiene un nombre unico y un valor asociado.
+# Para poder asignarle ese valor se utiliza el operador =
+
+# VALIDO: Si comienza con una letra y sin caracteres especiales
+mivariable = "Texto"
+
+# VALIDO: Si comienza con una letra y con guiones bajos intermedios
+mi_variable = "Texto"
+
+# VALIDO: Podemos comenzar con _
+_mi_variable = "Texto"
+
+# VALIDO: Puede comenzar y ser todo mayuscula
+MIVARIABLE = "Texto"
+
+# VALIDO: Podemos utilizar numeros pero no al comienzo
+mivariable2 = "Texto"
+
+# NO VALIDO: Comenzar con numeros
+
+# NO VALIDO: Usar - guion medio
+
+# NO VALIDO: Usar espacios
+
+# NO VALIDO: Usar simbolo $
