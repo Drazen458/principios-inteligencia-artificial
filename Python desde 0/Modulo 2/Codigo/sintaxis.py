e835f29c0e736cd43cd2546afb9da0bc8286e5a6
Drazen458 <valentinbarberis211@gmail.com>
2025-01-28 20:50:34 -0300

    Add file

---
+++ Python desde 0/Modulo 2/Codigo/sintaxis.py
@@ -0,0 +1,13 @@
+# SINTAXIS DE PYTHON
+
+# Se refiere a las reglas que se deben seguir al escribir el codigo para que sea valido y pueda ser interpretado correctamente
+
+# INDENTACION:
+
+# La indentacion es el "espacio en blanco" llamada tabulacion o sangria al comienzo de una linea de codigo
+# El scope / alcance de una estructura no se delimitara por llaves como otros lenguajes sino a traves de la indentacion
+
+for i in range(5):
+    for j in range(5):
+        print(i)
+        print(j)
